9feef6241fc8b8f191c9deb423edc87ee6f84990
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-08-22 21:20:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
+const num1 = 31;
+const num2 = 2;
+const num3 = 24;
+const num4 = 5;
 // Write your code here
+const multiply = (num1*num2);
+const random = 2;
+Math.random()*1;
+const mod = (num%num4);
+const max = Math.max(7,10,14,20);
